3463d0ac9b1ae97d578b7730f8575dcae301705b
kiven <superliunique@gmail.com>
2018-06-06 08:32:33 +0800

    jni 调通，等待新增ffmpeg库

---
+++ MediaCodec/src/main/cpp/myplayer.cpp
@@ -0,0 +1,29 @@
+#include <jni.h>
+#include <string>
+#include "android/log.h"
+
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, "ffmpeg jni", __VA_ARGS__);
+extern "C" {
+//#include <libavcodec/avcodec.h>
+//#include <libavformat/avformat.h>
+}
+
+extern "C" JNIEXPORT jstring
+
+JNICALL
+Java_com_dali_daliplayer_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_dali_daliplayer_FFmpegJni_play(JNIEnv *env, jclass type, jstring path_) {
+    const char *path = env->GetStringUTFChars(path_, 0);
+
+    // TODO
+    LOGI("path = %s", path);
+//    av_register_all();
+    env->ReleaseStringUTFChars(path_, path);
+}
